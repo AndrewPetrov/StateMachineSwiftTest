e8a7e15938a249e3c8b634523da438d448712abc
AndrewPetrov <andrew.petrov@yalantis.com>
2016-04-27 19:01:57 +0300

    [Refactor] Code refactored. Added FSM delegate

--- StateMachineSwiftTest/ViewController.swift
+++ StateMachineSwiftTest/ViewController.swift
@@ -8,25 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol UpdateView: class {
+    func updateView(level: Double)
+}
 
+class ViewController: UIViewController {
+    
     @IBOutlet weak var doneGoalsCount: UIStepper!
     @IBOutlet weak var label: UILabel!
     var waterView: WaterView!
+    lazy var goalsModel: GoalsModel = GoalsModel(delegate: self)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let inset: CGFloat = 100.0
         waterView = WaterView(frame: CGRectMake(0, inset / 2, view.frame.size.width, view.frame.size.height - inset * 2))
         view.addSubview(waterView)
     }
-
-
+    
     @IBAction func setDoneGoals(sender: UIStepper) {
-        waterView.doneGoalsCount = Int(sender.value)
-        label.text = "\(Int(sender.value)) / \(Int(sender.maximumValue))"
+        goalsModel.doneGoalsCount = Int(sender.value)
+        label.text = "\(goalsModel.level)"
     }
     
 }
 
+extension ViewController: UpdateView {
+    func updateView(level: Double) {
+        waterView.level = level
+    }
+}
+
+class GoalsModel {
+
+    weak private var delegate: UpdateView!
+    var level: Double {
+        return Double(doneGoalsCount) / Double(totalGoalsCount)
+    }
+    var doneGoalsCount = 0 {
+        didSet {
+            delegate.updateView(level)
+        }
+    }
+    private var totalGoalsCount = 10
+    
+    init(delegate: UpdateView) {
+        self.delegate = delegate
+    }
+    
+}
+
+extension UIColor {
+    
+    convenience init(rgbColorCodeRed red: Int, green: Int, blue: Int, alpha: CGFloat) {
+        let redPart = CGFloat(red) / 255
+        let greenPart = CGFloat(green) / 255
+        let bluePart = CGFloat(blue) / 255
+        
+        self.init(red: redPart, green: greenPart, blue: bluePart, alpha: alpha)
+    }
+}
--- StateMachineSwiftTest/WaterView.swift
+++ StateMachineSwiftTest/WaterView.swift
@@ -10,12 +10,8 @@ import Foundation
 import UIKit
 
 let wavesCount = 1
-let wavesChangingLevelDuration = 4.0
+let wavesChangingLevelDuration = 7.0
 let wavesChangingLevelDelay = 2.0
-
-enum LTWavesState {
-    case Stand, IncreasingAmplitude, ChangingLevel, DecreasingAmplitude
-}
 
 struct WaveAmplitudes {
     let minAmplitude: Int
@@ -23,96 +19,36 @@ struct WaveAmplitudes {
     let maxAmplitude: Int
 }
 
+protocol WaterViewDelegate: class {
+    func increaseWaves()
+    func decreaseWaves()
+    func changeWaterLevel()
+    func changeWavesColor()
+}
+
 class WaterView: UIView {
     
-    var totalGoalsCount = 5
-    
-    private var _doneGoalsCount: Int?
-    var doneGoalsCount: Int {
-        set {
-            self._doneGoalsCount = newValue
-            updateWavesLevel()
+    var level = 0.0 {
+        didSet {
+            delegate.changeLevel()
         }
-        get {
-            return self._doneGoalsCount!
-        }
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setupWavesView()
-        doneGoalsCount = 1
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     private var fluidViews: [BAFluidView]!
     private let lowWaves = WaveAmplitudes(minAmplitude: 1, amplitudeIncrement: 2, maxAmplitude: 5)
     private let highWaves = WaveAmplitudes(minAmplitude: 10, amplitudeIncrement: 3, maxAmplitude: 30)
-    private var previousWavesStoppedLevel: CGFloat!
-    private var wavesState: LTWavesState = .Stand
+    private var previousWavesStoppedLevel: Double!
     
-    func updateWavesLevel() {
-        if totalGoalsCount == 0 {
-            return
-        }
-        switch wavesState {
-            
-        case .Stand:
-            fallthrough
-        case .DecreasingAmplitude:
-            increaseWaves()
-            break;
-            
-        case .IncreasingAmplitude:
-            break;
-        case .ChangingLevel:
-            changeWaterLevel()
-            break;
-        }
+    private lazy var delegate: FSM = FSM(delegate: self)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
+        setupWavesView()
     }
     
-    private func increaseWaves() {
-        wavesState = .IncreasingAmplitude
-        
-        changeWavesAmplitudeTo(highWaves)
-        NSTimer.scheduledTimerWithTimeInterval(wavesChangingLevelDelay, repeats: false) {
-            self.changeWaterLevel()
-        }
-    }
-    
-    private func changeWaterLevel() {
-        var level = max(Float(doneGoalsCount) / Float(totalGoalsCount), 0.1)
-        level = min(Float(doneGoalsCount) / Float(totalGoalsCount), 0.9)
-        wavesState = .ChangingLevel
-        for fluidView in fluidViews {
-            fluidView.fillTo(NSNumber(float: level))
-        }
-    }
-    
-    private func decreaseWaves() {
-        if wavesState != .ChangingLevel {
-            return
-        }
-        self.wavesState = .DecreasingAmplitude
-        
-        NSTimer.scheduledTimerWithTimeInterval(wavesChangingLevelDelay,
-                                               repeats: false) { [weak self] in
-                                                if self!.wavesState == .DecreasingAmplitude {
-                                                    
-                                                    self!.changeWavesAmplitudeTo(self!.lowWaves)
-                                                    
-                                                    if self!.doneGoalsCount == self!.totalGoalsCount && self!.doneGoalsCount > 0 {
-                                                        self!.showCheckmark()
-                                                    }
-                                                    self!.wavesState = .Stand
-                                                    self!.previousWavesStoppedLevel = CGFloat(self!.doneGoalsCount) / CGFloat(self!.totalGoalsCount)
-                                                }
-        }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func setupWavesView() {
@@ -129,7 +65,7 @@ class WaterView: UIView {
             fluidView.fillRepeatCount = 0
             fluidView.fillDuration = wavesChangingLevelDuration
             fluidView.complitionBlock = { [weak self] in
-                self!.decreaseWaves()
+                self!.delegate.currentState = .DecreasingAmplitude
             }
             insertSubview(fluidView, atIndex: 0)
             fluidViews.append(fluidView)
@@ -145,7 +81,146 @@ class WaterView: UIView {
         }
     }
     
+    func changeWavesColor() {
+        var color: UIColor
+        switch  delegate.currentState {
+            
+        case .Idle:
+            color = UIColor(rgbColorCodeRed: 6, green: 37, blue: 255, alpha: 1)
+            
+        case .IncreasingAmplitude:
+            color = UIColor(rgbColorCodeRed: 2, green: 255, blue: 20, alpha: 1)
+            
+        case .ChangingLevel:
+            color = UIColor(rgbColorCodeRed: 255, green: 36, blue: 8, alpha: 1)
+            
+        case .DecreasingAmplitude:
+            color = UIColor(rgbColorCodeRed: 6, green: 231, blue: 232, alpha: 1)
+        }
+        
+        for fluidView in fluidViews {
+            UIView.animateWithDuration(0.2, animations: {
+                fluidView.fillColor = color
+            })
+        }
+    }
+    
     private func showCheckmark() {
         print("showCheckmark")
     }
 }
+
+extension WaterView: WaterViewDelegate {
+    func increaseWaves() {
+        changeWavesAmplitudeTo(highWaves)
+        NSTimer.scheduledTimerWithTimeInterval(wavesChangingLevelDelay, repeats: false) {
+            self.delegate.currentState = .ChangingLevel
+        }
+    }
+    
+    func changeWaterLevel() {
+        var safeLevel = max(level, 0.01)
+        safeLevel = min(level, 0.99)
+        for fluidView in fluidViews {
+            fluidView.fillTo(NSNumber(double: safeLevel))
+        }
+    }
+    
+    func decreaseWaves() {
+        NSTimer.scheduledTimerWithTimeInterval(
+            wavesChangingLevelDelay,
+            repeats: false) { [weak self] in
+                if self!.delegate.currentState == .DecreasingAmplitude {
+                    self!.changeWavesAmplitudeTo(self!.lowWaves)
+                    
+                    self!.delegate.currentState = .Idle
+                    self!.previousWavesStoppedLevel = self!.level
+                }
+        }
+    }
+}
+
+enum WavesState {
+    case Idle, IncreasingAmplitude, ChangingLevel, DecreasingAmplitude
+}
+
+class FSM {
+    
+    init(delegate: WaterViewDelegate) {
+        currentState = WavesState.Idle
+        self.delegate = delegate
+    }
+    
+    private var _currentState = WavesState.Idle
+    var currentState: WavesState {
+        set {
+            var shouldChangeState = false
+            switch (self.currentState, newValue){
+                
+            case (.Idle, .IncreasingAmplitude):
+                shouldChangeState = true
+                
+            case (.IncreasingAmplitude, .ChangingLevel):
+                shouldChangeState = true
+                
+            case (.ChangingLevel, .DecreasingAmplitude), (.ChangingLevel, .ChangingLevel):
+                shouldChangeState = true
+                
+            case (.DecreasingAmplitude, .Idle), (.DecreasingAmplitude, .IncreasingAmplitude):
+                shouldChangeState = true
+                
+            default:
+                break
+            }
+            if shouldChangeState {
+                _currentState = newValue
+                print("currentState = \(_currentState)")
+                animateView()
+            }
+        }
+        
+        get {
+            return _currentState
+        }
+    }
+    
+    weak var delegate: WaterViewDelegate!
+    
+    func changeLevel() {
+        switch (self.currentState){
+            
+        case (.Idle):
+            self.currentState = .IncreasingAmplitude
+            
+        case (.ChangingLevel):
+            self.currentState = .ChangingLevel
+            
+        case (.DecreasingAmplitude):
+            self.currentState = .IncreasingAmplitude
+            
+        default:
+            break
+        }
+        
+        
+    }
+    
+    private func animateView() {
+        switch currentState {
+            
+        case .Idle:
+            break
+            
+        case .DecreasingAmplitude:
+            delegate.decreaseWaves()
+            
+        case .IncreasingAmplitude:
+            delegate.increaseWaves()
+            
+        case .ChangingLevel:
+            delegate.changeWaterLevel()
+        }
+        delegate.changeWavesColor()
+    }
+}
+
